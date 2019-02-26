c58c4a6a4a95574d8539b2a4a8a1883699da33f0
Jacob <jelondon12@gmail.com>
2019-02-26 09:12:13 -0800

    added cprofile to test to have detailed timing stats

--- Implementation/Python/test/test.py
+++ Implementation/Python/test/test.py
@@ -1,53 +1,58 @@
 
 from alg import *
-import random, time
+import random, time, cProfile
 
-dist_threshold = 40
+def main():
 
-start = time.time()
+    dist_threshold = 40
 
-# create a database and a set of words to find
-database = create_sequence(length=1000)
-words = generate_words(amount=3, length=9)
-# get all permutations of those words and look for them
-perms = permute_words(words)
-perm_time = time.time()
+    start = time.time()
 
-#matches = noregex_locate(perms, database)
-matches = locate(perms, database)
-match_time = time.time()
+    # create a database and a set of words to find
+    database = create_sequence(length=1000)
+    words = generate_words(amount=3, length=9)
+    # get all permutations of those words and look for them
+    perms = permute_words(words)
+    perm_time = time.time()
 
-# matches: list(tuple('dna', list(indices)))
+    #matches = noregex_locate(perms, database)
+    matches = locate(perms, database)
+    match_time = time.time()
 
-# sort the matches by their index
-print('\nMatches:')
-matches = sorted(matches.items(), key=lambda v: v[1])
-sort_time = time.time()
-"""for match in matches:
-    print(str(matches[0][0]))"""
-print(matches)
+    # matches: list(tuple('dna', list(indices)))
 
-# find pairs of candidates which are close
-candidates = []
-for c in range(len(matches) - 1):
+    # sort the matches by their index
+    print('\nMatches:')
+    matches = sorted(matches.items(), key=lambda v: v[1])
+    sort_time = time.time()
+    """for match in matches:
+        print(str(matches[0][0]))"""
+    print(matches)
 
-    first_indices = matches[c][1]
-    second_indices = matches[c + 1][1]
+    # find pairs of candidates which are close
+    candidates = []
+    for c in range(len(matches) - 1):
 
-    if second_indices[0] - first_indices[0] < dist_threshold:
-        candidates.append((matches[c], matches[c + 1]))\
+        first_indices = matches[c][1]
+        second_indices = matches[c + 1][1]
 
-candidate_time = time.time()
-print('\nCandidates:')
-for c in candidates:
-    print(c)
+        if second_indices[0] - first_indices[0] < dist_threshold:
+            candidates.append((matches[c], matches[c + 1]))\
+
+    candidate_time = time.time()
+    print('\nCandidates:')
+    for c in candidates:
+        print(c)
 
 
-end = time.time()
-total_time = end - start
-print('\nStats')
-print('Total time:', total_time)
-print('Permutation time:', perm_time - start, '\n', (perm_time - start) / total_time * 100)
-print('Match time:', match_time - perm_time, '\n', (match_time - perm_time) / total_time * 100)
-print('Sort time:', sort_time - match_time, '\n', (sort_time - match_time) / total_time * 100)
-print('Candidate time:', candidate_time - match_time, '\n', (candidate_time - match_time) / total_time * 100)
+    end = time.time()
+    total_time = end - start
+    print('\nStats')
+    print('Total time:', total_time)
+    print('Permutation time:', perm_time - start, '\n', (perm_time - start) / total_time * 100)
+    print('Match time:', match_time - perm_time, '\n', (match_time - perm_time) / total_time * 100)
+    print('Sort time:', sort_time - match_time, '\n', (sort_time - match_time) / total_time * 100)
+    print('Candidate time:', candidate_time - match_time, '\n', (candidate_time - match_time) / total_time * 100)
+
+if __name__ == '__main__':
+    cProfile.run('main()')
