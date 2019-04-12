2dd1024d28e55f30cf229aee95c50f13e02ddfd1
Jacob London <jelondon12@gmail.com>
2019-04-12 14:21:31 -0700

    changed splitting to output json with split sequences

--- Implementations/Python/util/setup_files.py
+++ Implementations/Python/util/setup_files.py
@@ -1,33 +1,66 @@
 
-import os, tqdm
+import os, tqdm, json
+from copy import copy
 
-def setup(path, sep='>'):
-    in_file = open(path, 'r')
+from split import split
 
-    # make a directory to output the files to
-    path = path[0:path.index('.')]
+thisfilepath = os.path.dirname(os.path.abspath(__file__))
 
-    print(f'Creating output directory: {path}')
+"""Given a .fa or .fasta file, turn it into .json format
+where each sequence name maps to its sequence
+"""
+def build_sequence(path: str, sep: str='>') -> dict:
+    seq_file = open(path, 'r')
 
-    try:
-        os.mkdir(path)
-    except OSError as e:
-        print(e)
-        response = input('Continue anyway? [y/n] ')
-        if response.lower() != 'y':
-            return
+    result = {} # {'sequence name' : 'sequence letters'}
+    name = ''
 
-    print('Building file list')
-
-    # read the lines into files
-    for line in tqdm.tqdm(in_file.readlines()):
+    # put all sequence letters into a single string for each sequence
+    for line in tqdm.tqdm(seq_file.readlines()):
+        # a sequence name is found
         if line[0] == sep:
-            out_file = open(path + '/' + line[1:line.index('\n')] + '.txt', 'w')
-            continue
-        
-        out_file.write(line)
+            # start after the seperator and remove newlines
+            name = copy(line.rstrip('\n\r')[len(sep):])
+            result[name] = []
+        # sequence letters are found
+        else:
+            # remove newlines and append
+            result[name].append(line.rstrip('\n\r'))
+
+    # change ['sequence'] -> 'sequence'
+    for name, sequence in result.items():
+        result[name] = sequence[0]
+
+    return result
+
+"""Given a dictionary of 'name' : 'sequence', split
+the sequence into words of a given length
+"""
+def split_sequence(data: dict, length: int=11) -> dict:
+    result = {}
+    
+    # traverse the sequence
+    for name, sequence in data.items():
+        result[name] = split(iterable=sequence, length=length)
+
+    return result
+
+"""Read in a .fa or .fasta file, split it by sequence
+name and split the sequence into words of a given length
+"""
+def setup_data(path: str, length: int=11, sep: str='>') -> None:
+    # read data to a dict 'name' : 'sequence'
+    string_seq = build_sequence(path, sep=sep)
+    # data -> 'name' : ['word', 'word', ...]
+    split_seq = split_sequence(string_seq, length=length)
+
+    # write to .json file
+    data_file = thisfilepath + os.sep + path + '.json'
+    with open(data_file, 'w') as d_json:
+        json.dump(split_seq, d_json, indent=0, separators=(',', ': '))
 
 if __name__ == '__main__':
     f_in = 'SRR7236689--ARG830.fa'
     #f_in = 'Gn-SRR7236689_contigs.fasta'
-    setup(f_in)
+    setup_data(f_in)
+        
--- Implementations/Python/util/split.py
+++ Implementations/Python/util/split.py
@@ -1,6 +1,6 @@
 
 """Split an iterable into all possible slices of given length"""
-def split(iterable, length):
+def split(iterable: iter, length: int) -> list:
     num_words = len(iterable) - length + 1
     words = []
     for i in range(num_words):
