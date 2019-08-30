c1ad9b977352d3277b0e449d30b8083eba4c3837
Jacob <jelondon12@gmail.com>
2019-08-30 11:17:27 -0700

    fixed match

--- cpp/lib/match.cpp
+++ cpp/lib/match.cpp
@@ -13,28 +13,35 @@ Blastn::MatchedSequenceMap match_filter(Blastn::IndexedSequenceMap query, Blastn
 			for (auto q_word_indices = q_name_wordmap->second.begin();
 					  q_word_indices != q_name_wordmap->second.end();
 					  ++q_word_indices) {
-				// the current word in the query is also in data but not inserted yet
-				if (d_name_wordmap->second.find(q_word_indices->first) == d_name_wordmap->second.end()) {
-					matches[q_name_wordmap->first] = std::vector<Match> {
-						Match { q_word_indices->first,
+
+				// ensure data has the query word
+				if (d_name_wordmap->second.find(q_word_indices->first) != d_name_wordmap->second.end()) {
+					// the current word in the query is also in data but not inserted yet
+					if (d_name_wordmap->second.find(q_word_indices->first) == d_name_wordmap->second.end()) {
+						matches[q_name_wordmap->first] = std::vector<Match> {
+							Match {
+								q_word_indices->first,
 								d_name_wordmap->second[q_word_indices->first],
 								q_word_indices->second
-						} // end of Match initializer
-					}; // end of vector<Match> initializer
+							} // end of Match initializer
+						}; // end of vector<Match> initializer
+					}
+					// the current word in the query is also in data, and has already been inserted
+					else {
+						matches[q_name_wordmap->first].emplace_back(
+							q_word_indices->first,
+							d_name_wordmap->second[q_word_indices->first],
+							q_word_indices->second
+						);
+					}
 				}
-				// the current word in the query is also in data, and has already been inserted
-				else {
-					matches[q_name_wordmap->first].emplace_back(
-						q_word_indices->first,
-						d_name_wordmap->second[q_word_indices->first],
-						q_word_indices->second
-					);
-				}
-			}
-		}
+			} // end word_indices
+		} // end query map
+
 		// record if there were matches found
 		if (!matches.empty())
 			exact_matches.insert(std::pair<std::string, Blastn::MatchedMatchesMap>{ d_name_wordmap->first, matches });
-	}
+	} // end data map
+
 	return exact_matches;
 }
