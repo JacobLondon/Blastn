069148ac59496b8d698971d2f58d2172190ae01a
Jacob <jelondon12@gmail.com>
2019-10-17 17:17:13 -0700

    cleaned up timer tool

--- cpp/util/display.cpp
+++ cpp/util/display.cpp
@@ -13,3 +13,27 @@ void Progress::update()
     if (iteration == max)
         std::cout << std::endl;
 }
+
+Timer::Timer(std::string program_name)
+    : program_name{program_name}
+{}
+
+void Timer::start()
+{
+    start_clock = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::system_clock::now();
+    auto start_timestamp = std::chrono::system_clock::to_time_t(start_time);
+
+    std::cout << program_name << " started on " << std::ctime(&start_timestamp) << std::endl;
+}
+
+void Timer::stop()
+{
+    auto end_clock = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> duration = end_clock - start_clock;
+    auto end_time = std::chrono::system_clock::now();
+    auto end_timestamp = std::chrono::system_clock::to_time_t(end_time);
+
+    std::cout << program_name << " finished on " << std::ctime(&end_timestamp)
+              << "Elapsed time: " << duration.count() << "s." << std::endl;
+}
--- cpp/util/display.hpp
+++ cpp/util/display.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <chrono>
+#include <ctime>
 
 class Progress {
 public:
@@ -7,4 +9,16 @@ public:
     size_t max = 0;
 
     void update();
+};
+
+class Timer {
+    using Clock = std::chrono::high_resolution_clock::time_point;
+public:
+    Timer(std::string program_name);
+
+    Clock start_clock;
+
+    std::string program_name;
+    void start();
+    void stop();
 };
