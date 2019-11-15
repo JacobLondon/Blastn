f09e2089aa98258408f560287c67f9d5fc39c37e
Jacob London <jelondon12@gmail.com>
2019-11-15 13:26:03 -0800

    score matrix of cells

--- hdl/Blastn.srcs/sources_1/new/score_matrix.vhd
+++ hdl/Blastn.srcs/sources_1/new/score_matrix.vhd
@@ -6,11 +6,14 @@ entity ScoreMatrix is
     Generic (
         g_SIZE     : POSITIVE := 1000;
         g_BITS     : POSITIVE := 32;
-        g_MATCH    : INTEGER  := 2;
-        g_MISMATCH : INTEGER  := -1;
-        g_GAP      : INTEGER  := -1
     );
     Port (
+        clk         : in  STD_LOGIC;
+        rst         : in  STD_LOGIC;
+        go          : in  STD_LOGIC;
+        i_match     : in  SIGNED(1 downto 0);
+        i_mismatch  : in  SIGNED(1 downto 0);
+        i_gap       : in  SIGNED(1 downto 0);
         i_length    : in  UNSIGNED(g_BITS - 1 downto 0);         -- length of the query or the subject
         i_gap_start : in  UNSIGNED(g_BITS - 1 downto 0);         -- gap start index in the query
         i_gap_count : in  UNSIGNED(g_BITS - 1 downto 0);         -- number of gaps in the query
@@ -20,15 +23,15 @@ entity ScoreMatrix is
     );
 end ScoreMatrix;
 
-architecture BlastnRTL of ScoreMatrix is
+architecture Blastn of ScoreMatrix is
 
     component Cell is
         Port (
             s        : in  STD_LOGIC_VECTOR(1 downto 0);
             q        : in  STD_LOGIC_VECTOR(2 downto 0);
-            match    : in  STD_LOGIC_VECTOR(1 downto 0);
-            mismatch : in  STD_LOGIC_VECTOR(1 downto 0);
-            gap      : in  STD_LOGIC_VECTOR(1 downto 0);
+            match    : in  SIGNED(1 downto 0);
+            mismatch : in  SIGNED(1 downto 0);
+            gap      : in  SIGNED(1 downto 0);
             diag     : in  STD_LOGIC_VECTOR(1 downto 0);
             up       : in  STD_LOGIC_VECTOR(1 downto 0);
             left     : in  STD_LOGIC_VECTOR(1 downto 0);
@@ -40,23 +43,48 @@ architecture BlastnRTL of ScoreMatrix is
     
     signal m_score_matrix : MATRIX := (others => (others => '0'));
 
+    -- Smith-Waterman score counter
+    -- refer to page 4 of https://arxiv.org/pdf/1803.02657.pdf
+
+    signal r_score : UNSIGNED(g_BITS - 1 downto 0) := (others => '0');
+    signal r_en    : STD_LOGIC;
+    signal r_dis   : STD_LOGIC;
+
 begin
+
+    r_en <= go;
+    r_dis <= '1' when unsigned(m_score_matrix(g_SIZE - 1)(g_SIZE - 1 downto g_SIZE - g_BITS)) > 0 else '0';
+
+    SCORE_COUNTER: process(clk) is
+    begin
+        if rising_edge(clk) then
+            -- synchronous reset
+            if rst = '1' then
+                r_score <= (others => '0');
+                r_en    <= '1';
+                r_dis   <= '0';
+            -- only increment the score if the
+            elsif r_en = '1' and r_dis = '0' then
+                r_score = r_score + 1;
+            end if;
+        end if;
+    end process;
 
     ROW: for i in 1 to g_SIZE - 1 generate
         COLUMN: for j in 1 to g_SIZE / 2 - 1 generate
-            current_cell: Cell
+            CURRENT_CELL: Cell
                 Port map (
-                    s => i_subject(i),
-                    q => i_query(j*2),
-                    match => std_logic_vector(g_MATCH),
-                    mismatch => std_logic_vector(g_MISMATCH),
-                    gap => std_logic_vector(g_GAP),
-                    diag => "11",
-                    up => "11",
-                    left => "11",
-                    score => open
+                    s        => i_subject(i),
+                    q        => i_query(j*2),
+                    match    => i_MATCH,
+                    mismatch => i_MISMATCH,
+                    gap      => i_GAP,
+                    diag     => m_score_matrix(i - 1)(j*2 - 1 downto j*2 - 2),
+                    up       => m_score_matrix(i - 1)(j*2 + 1 downto j*2    ),
+                    left     => m_score_matrix(i    )(j*2 - 1 downto j*2 - 2),
+                    score    => m_score_matrix(i    )(j*2 + 1 downto j*2    )
                 );
         end generate;
     end generate;
 
-end BlastnRTL;
+end Blastn;
